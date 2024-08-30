47771d575394924c2ad39667c4a5162bacd6300b
Zuhal <77770731+zuhalcode@users.noreply.github.com>
2024-08-30 21:48:24 +0700

    prisma client

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
   findAll() {
-    return 'Hello World adding prisma in package no vercel';
+    return 'Hello World adding prisma in package no vercel prisma client';
   }
 }
