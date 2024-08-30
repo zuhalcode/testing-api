c76d3f2d3733f4a93990cbdb5a68879eb869e072
Zuhal <77770731+zuhalcode@users.noreply.github.com>
2024-08-30 21:24:52 +0700

    delete prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UserModule],
 })
 export class AppModule {}
